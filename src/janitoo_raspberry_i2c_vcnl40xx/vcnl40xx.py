7e2966d43a221df517f7fbdc85b37d112a10844f
bibi21000 <bibi21000@gmail.com>
2016-05-02 18:07:55 +0200

    Updates for tests

--- src/janitoo_raspberry_i2c_vcnl40xx/vcnl40xx.py
+++ src/janitoo_raspberry_i2c_vcnl40xx/vcnl40xx.py
@@ -74,9 +74,8 @@ class VCLN4000Component(JNTComponent):
         name = kwargs.pop('name', "Input")
         product_name = kwargs.pop('product_name', "VCLN4000 proximity detector")
         product_type = kwargs.pop('product_type', "VCLN4000 proximity detector")
-        product_manufacturer = kwargs.pop('product_manufacturer', "Janitoo")
         JNTComponent.__init__(self, oid=oid, bus=bus, addr=addr, name=name,
-                product_name=product_name, product_type=product_type, product_manufacturer="Janitoo", **kwargs)
+                product_name=product_name, product_type=product_type, **kwargs)
         logger.debug("[%s] - __init__ node uuid:%s", self.__class__.__name__, self.uuid)
 
         uuid="addr"
